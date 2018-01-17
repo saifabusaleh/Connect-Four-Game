318c56ddbe1e81a2cdbd0cd1b66c1f9eaa82ca55
cracker <lolzak4@gmail.com>
2018-01-17 22:50:31 +0200

    Add login and signup window

--- ConnectFourClient/ConnectFourClient/LoginWindow.xaml.cs
+++ ConnectFourClient/ConnectFourClient/LoginWindow.xaml.cs
@@ -12,7 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
-
+using ConnectFourClient.ConnectFourService;
 namespace ConnectFourClient
 {
     /// <summary>
@@ -20,9 +20,30 @@ namespace ConnectFourClient
     /// </summary>
     public partial class LoginWindow : Window
     {
+        ConnectFourServiceClient connection;
         public LoginWindow()
         {
             InitializeComponent();
+            connection = new ConnectFourServiceClient();
+        }
+
+        private void Login_Click(object sender, RoutedEventArgs e)
+        {
+            string username = tbUsername.Text;
+            string password = tbPassword.Text;
+            if(string.IsNullOrEmpty(username) || string.IsNullOrEmpty(password))
+            {
+                MessageBox.Show("username and password cant be empty!");
+                return;
+            }
+            bool res = connection.login(username, password);
+            MessageBox.Show(res.ToString());
+        }
+
+        private void Signup_Click(object sender, RoutedEventArgs e)
+        {
+            SignupWindow window = new SignupWindow();
+            window.Show();
         }
     }
 }
--- ConnectFourClient/ConnectFourClient/SignupWindow.xaml.cs
+++ ConnectFourClient/ConnectFourClient/SignupWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ConnectFourClient.ConnectFourService;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -19,9 +20,26 @@ namespace ConnectFourClient
     /// </summary>
     public partial class SignupWindow : Window
     {
+        ConnectFourServiceClient connection;
+
         public SignupWindow()
         {
             InitializeComponent();
+            connection = new ConnectFourServiceClient();
+        }
+
+        private void Signup_Click(object sender, RoutedEventArgs e)
+        {
+            string username = tbUsername.Text;
+            string password = tbPassword.Text;
+            if (string.IsNullOrEmpty(username) || string.IsNullOrEmpty(password))
+            {
+                MessageBox.Show("username and password cant be empty!");
+                return;
+            }
+            connection.register(username, password);
+
+            MessageBox.Show("Register done successfully!");
         }
     }
 }
